086ddfa47e3e04f4a45943d6d6e88b5863d025d2
김현우 <forsolid7@naver.com>
2026-01-12 12:49:07 +0900

    day03: input and age calculation

---
+++ day03_input_age.py
@@ -0,0 +1,10 @@
+name = input("enter your name:")
+birth_year = input("enter your birth year:")
+
+birth_year = int(birth_year)
+current_year = 2026
+
+age = current_year - birth_year
+
+print("name:", name)
+print("age", age)
